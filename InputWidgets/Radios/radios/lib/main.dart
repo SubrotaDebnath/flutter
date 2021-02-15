eb92d1bff7d10dc1a1ec29041acb3f590670c98a
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-02-15 23:54:42 +0600

    radio

--- InputWidgets/Radios/radios/lib/main.dart
+++ InputWidgets/Radios/radios/lib/main.dart
@@ -10,6 +10,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  var _value1 =0;
+  var _value2 = 0;
+
+  _setValue1(var value){
+    setState(() {
+      _value1 = value;
+    });
+  }
+
+  _setValue2(var value){
+    setState(() {
+      _value2 = value;
+    });
+  }
+
+  Widget makeRadios() {
+    var list = [];
+
+    for(int i=0; i<4; i++){
+      list.add(Radio(value: i, groupValue: _value1, onChanged: _setValue1));
+    }
+
+    Column column = Column(children:list);
+
+    return column;
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +50,7 @@ class _MyAppState extends State<MyApp> {
           child: Center(
             child: Column(
               children: [
-                
+                makeRadios(),
               ],
             ),
           ),
